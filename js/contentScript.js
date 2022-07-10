f583d5935037e4e279ebf058a42331973b225492
Ryan <ryan.sawchuk@dal.ca>
2022-07-09 19:13:52 -0700

    Fix event listners

--- js/contentScript.js
+++ js/contentScript.js
@@ -1,10 +1,40 @@
 
-window.addEventListener("onhashchange", function(){
-    //url hash # has changed
-    console.log("URL CHANGES")
+var lastUrl = "";
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.message === "URLUpdate"){
+        //console.log(`LAST: ${lastUrl}, NEW: ${request.url}`)
+        if (lastUrl === "" || lastUrl != request.url){
+            lastUrl = request.url;
+            
+            beginSentimentProcess();
+            console.log(document.title)
+            sendResponse({ message: `Starting sentiment process.` });
+        }
+        else {
+            sendResponse({ message: "No new YouTube page." });
+        }
+    }
 });
 
-//alert("hello");
-console.log(window.location.href)
-console.log(document.URL)
+function beginSentimentProcess(){
+    collectCommentData();
+
+    computeSentiment();
+
+    displaySentimentVisual();
+}
+
+function collectCommentData(){
+
+}
+
+function computeSentiment(){
+
+}
+
+function displaySentimentVisual(){
+
+}
+
 console.log('contentScript.js ran.')
