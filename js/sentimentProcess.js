f921a9ebde3e4a8ca2f96d209d935c66da58ec7d
ryan <ryan.sawchuk@dal.ca>
2022-12-28 12:21:19 -0700

    Refactor data collection

--- js/sentimentProcess.js
+++ js/sentimentProcess.js
@@ -1,13 +1,11 @@
 
 async function beginSentimentProcess(videoId){
-    console.log("START SentimentProcess");
-
     for (var i = 0; i < 10; i++){
-        await queryCommentData(videoId);
+        var comments = await queryCommentData(videoId);
 
-        await computeSentiment();
+        await computeSentiment(comments);
 
-        await displaySentimentVisual();
+        //await displaySentimentVisual();
 
         break;
     }
@@ -15,16 +13,50 @@ async function beginSentimentProcess(videoId){
 
 async function queryCommentData(videoId){    
     const youtubeAPIKey = "AIzaSyAIMOdtojUDvFctt0xfoqcRYXqztBqASKw";
-    var URL = `https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet&maxResults=100&order=relevance&videoId=${videoId}&key=${youtubeAPIKey}`
+    var URL = `https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&maxResults=100&order=relevance&videoId=${videoId}&key=${youtubeAPIKey}`
+
+    var result = {};
 
     fetch(URL, headers = { "Content-Type": "application/json" })
-        .then(response => response.json())
-        .then((data) => console.log(data))
-        .catch(err => console.log(err))
+    .then(response => response.json())
+    .then((data) => {
+        if (!data) { throw new Error("Empty response."); }
+
+        result['totalResults'] = data['pageInfo']['totalResults'];
+        result['nextPageToken'] = data['nextPageToken'];
+
+        result['comments'] = [];
+
+        for (var index in data.items){
+            extractedData = {
+                "comment": data.items[index]['snippet']['topLevelComment']['snippet']['textOriginal'],
+                "likes": data.items[index]['snippet']['topLevelComment']['snippet']['likeCount'],
+                "author": data.items[index]['snippet']['topLevelComment']['snippet']['authorDisplayName'],
+                "replies": []
+            };
+
+            if (data.items[index]['replies']){
+                for (var replyIndex in data.items[index]['replies'].comments){
+                    extractedData["replies"].push({
+                        "comment": data.items[index]['replies'].comments[replyIndex]['snippet']['textOriginal'],
+                        "likes": data.items[index]['replies'].comments[replyIndex]['snippet']['likeCount'],
+                        "author": data.items[index]['replies'].comments[replyIndex]['snippet']['authorDisplayName']
+                    });
+                }
+            }
+
+            result['comments'].push(extractedData);
+        }
+    })
+    .catch(err => console.log(err))
+    console.log(result)
+    return result;
 }
 
-async function computeSentiment(){
-
+async function computeSentiment(comments){
+    for (var index in comments.comments){
+        comments.comments[index]
+    }
 }
 
 async function displaySentimentVisual(){
---
+++ support/build_word_list.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import json
+
+worldlist = pd.read_csv('LoughranMcDonald_MasterDictionary_2018.csv')
+
