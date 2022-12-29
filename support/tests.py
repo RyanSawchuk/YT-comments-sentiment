d3ff9b9fe8c24bae9fa6aad6119a26782f622e84
ryan <ryan.sawchuk@dal.ca>
2022-12-28 20:29:45 -0700

    Rework sentiment word score tests

--- support/tests.py
+++ support/tests.py
@@ -7,3 +7,5 @@ score = afinn.score("Bob is a Positively Inspirational and Motivating Person!")
 print(score)
 
 'https://raw.githubusercontent.com/fnielsen/afinn/master/afinn/data/AFINN-111.txt'
+
+'https://github.com/nealcaren/osscabd_2018/blob/master/notebooks/data/nyt_201704_comments.json?raw=true'
