64df984f9509b232076e45049cdebb056498b7e3
ryan <ryan.sawchuk@dal.ca>
2022-12-30 21:40:21 -0700

    Clean stale code

--- src/background.js
+++ src/background.js
@@ -3,15 +3,3 @@
 chrome.runtime.onInstalled.addListener(() => {
   console.log('Extension installed');
 });
-
-chrome.tabs.onUpdated.addListener((tabId, info, tab) => {
-  if (tab.url.includes("www.youtube.com/watch?v=")){
-      chrome.tabs.sendMessage(tabId, 
-          { message: "URLUpdate", url: tab.url, title: tab.title }, 
-          function(response) { 
-              console.log(response.message); 
-          }
-      );
-  }
-});
-
--- src/contentScript.js
+++ src/contentScript.js
@@ -4,16 +4,3 @@ const pageTitle = document.head.getElementsByTagName('title')[0].innerHTML;
 console.log(
   `Page title is: '${pageTitle}' - evaluated by Chrome extension's 'contentScript.js' file`
 );
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.message === "URLUpdate"){
-      if (lastUrl === "" || lastUrl != request.url){
-          lastUrl = request.url;
-          sendResponse({ message: `Starting sentiment process.` });
-          //beginSentimentProcess(title);
-      }
-      else {
-          sendResponse({ message: "No new YouTube page." });
-      }
-  }
-});
